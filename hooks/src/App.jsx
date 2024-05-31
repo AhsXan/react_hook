70fc38de95cf51edc991e267d4f7b2eea10dcf11
Ahsan Ahmed <162038567+AhsXan@users.noreply.github.com>
2024-05-31 15:43:14 +0000

    states

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 
 function App() {
-  let counter = 10;
+  let [counter, setCounter] = useState(3)
   const addvalue = () => {
-    counter++;
+    if (counter < 10)
+      setCounter(counter + 1)
   }
+  const removeValue = () => {
+    if (!counter <= 0)
+      setCounter(counter - 1)
+  }
+    useEffect(() => {
+      alert("On First Render")
+
+
+    }, [])
+ 
+    useEffect(()=>{
+      alert("On Every Render")
+
+    })
+    
+    useEffect(()=>{
+      alert("On changing counter")
+
+    },[counter])
+
   return (
 
     <>
@@ -15,7 +36,7 @@ function App() {
       <h2>Counter Value {counter}</h2>
       <button onClick={addvalue}>Add Value</button>
       <br />
-      <button>Remove Value</button>
+      <button onClick={removeValue}>Remove Value</button>
     </>
 
   )
